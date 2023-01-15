f8240c83362296d88dbc4878166d1be51bc1aac2
mcfurry <mcfurry.only@gmail.com>
2023-01-15 20:55:42 +0300

    arrays homework exercise 4

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int [] weights = {1, 2, 3,};
+        int [] weights = {1, 2, 3};
 
-
-        double [] weight = {1.57, 7.654, 9.986};
-        int [] number = {33, 25, 11, 10};
-
-        for (int i = weights.length - 1; i >= 0; i--) {
-            System.out.print(weights[i]);
-            if (i != 0) {
-                System.out.print(", ");
+        for (int i = 0; i < weights.length; i++) {
+            if ( weights[i] % 2 != 0){
+                weights[i] += 1;
             }
         }
-        System.out.println();
-
-        for (int i = weight.length - 1; i >= 0; i--) {
-            System.out.print(weight[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-
-        for (int i = number.length - 1; i >= 0; i--) {
-            System.out.print(number[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-
+        System.out.println(Arrays.toString( weights));
     }
  }
 
